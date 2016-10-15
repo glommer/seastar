6f1f60b866bd0a1bd48dccf11416ca412a47fe7f
Glauber Costa <glauber@scylladb.com>
2016-10-15 13:21:18 -0400

    semaphore: add test for negative counters
    Signed-off-by: Glauber Costa <glauber@scylladb.com>

--- tests/semaphore_test.cc
+++ tests/semaphore_test.cc
@@ -44,6 +44,22 @@ SEASTAR_TEST_CASE(test_semaphore_wait_0) {
     return make_ready_future<>();
 }
 
+SEASTAR_TEST_CASE(test_semaphore_consume) {
+    semaphore sem(0);
+    sem.consume(1);
+    BOOST_REQUIRE_EQUAL(sem.current(), 0);
+    BOOST_REQUIRE_EQUAL(sem.waiters(), 0);
+
+    BOOST_REQUIRE_EQUAL(sem.try_wait(0), false);
+    auto fut = sem.wait(0);
+    BOOST_REQUIRE_EQUAL(fut.available(), false);
+    BOOST_REQUIRE_EQUAL(sem.waiters(), 1);
+    sem.signal_one(1);
+    BOOST_REQUIRE_EQUAL(sem.waiters(), 0);
+    return make_ready_future<>();
+}
+
+
 SEASTAR_TEST_CASE(test_semaphore_1) {
     return do_with(std::make_pair(semaphore(0), 0), [] (std::pair<semaphore, int>& x) {
         x.first.wait().then([&x] {
